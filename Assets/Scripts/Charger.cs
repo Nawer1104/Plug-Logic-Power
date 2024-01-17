48c68691429239694796f105f8f352d8edbbadf2
SteveNguyen <nam.ngh2211@mail.com>
2024-01-17 11:21:20 +0700

    Init

---
+++ Assets/Scripts/Charger.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charger : MonoBehaviour
+{
+    public Enum status;
+
+    public GameObject vfxCharge;
+
+    public GameObject vfxTouch;
+
+    public GameObject vfxFail;
+
+    Vector2 startPos;
+
+    private void Awake()
+    {
+        startPos = transform.position;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null && collision.gameObject.CompareTag("Pin"))
+        {
+            status = Enum.FullyCharge;
+
+            GameObject vfx = Instantiate(vfxTouch, transform.position, Quaternion.identity) as GameObject;
+
+            Destroy(vfx, 1f);
+
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision != null && collision.gameObject.CompareTag("Phone"))
+        {
+            if (status != Enum.FullyCharge)
+            {
+                GameObject vfx = Instantiate(vfxFail, transform.position, Quaternion.identity) as GameObject;
+
+                Destroy(vfx, 1f);
+
+                transform.position = startPos;
+
+                GetComponent<DragAndDrop>()._dragging = false;
+
+                return;
+            }
+            else
+            {
+                GetComponent<DragAndDrop>()._dragging = false;
+                GameObject vfx = Instantiate(vfxCharge, transform.position, Quaternion.identity) as GameObject;
+
+                Destroy(vfx, 1f);
+
+                gameObject.SetActive(false);
+                collision.gameObject.SetActive(false);
+                GameManager.Instance.levels[GameManager.Instance.GetCurrentIndex()].gameObjects.Remove(collision.gameObject);
+                GameManager.Instance.CheckLevelUp();
+            }
+        }
+    }
+}
